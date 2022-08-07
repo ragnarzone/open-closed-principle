d1ed577ffc5ec22d4b0ba86621a587da14f8188e
ragnarzone <ragnarzone@gmail.com>
2022-08-07 15:05:00 +0300

    Initial commit

---
+++ src/test/java/com/ragnarzone/openclosedprinciple/OpenClosedPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarzone.openclosedprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenClosedPrincipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
